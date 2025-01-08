5d004ce786cc1bbb4b32e05ce0415b5aa450a3e5
William Matthews <will@avos.io>
2025-01-08 12:03:04 +0000

    de-slop

--- tpp_test.go
+++ tpp_test.go
@@ -3,8 +3,8 @@ package tpp
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
 
 type mockImpl struct {
@@ -17,40 +17,32 @@ func (m *mockImpl) DoSomething(x int) bool {
 }
 
 func TestUnexpectedWithArgumentMatcher(t *testing.T) {
+	is := require.New(t)
 	mockObj := new(mockImpl)
 
 	// Create an argument matcher
 	isEven := func(x int) bool {
 		return x%2 == 0
 	}
-	matcher := mock.MatchedBy(isEven)
+	argMatcher := mock.MatchedBy(isEven)
+	call := mockObj.On("DoSomething", argMatcher).Return(true)
 
-	// Set up the mock with the argument matcher
-	call := mockObj.On("DoSomething", matcher).Return(true)
-
-	// Create an unexpected expectation
 	unexpected := Unexpected()
-
-	// This should work, as we safely Unset.
 	unexpected.Expectorise(call)
 
-	// Verify that the call was properly unset
 	mockObj.AssertExpectations(t)
-	assert.Empty(t, mockObj.ExpectedCalls)
+	is.Empty(mockObj.ExpectedCalls)
 }
 
 func TestUnexpected(t *testing.T) {
+	is := require.New(t)
 	mockObj := new(mockImpl)
 
 	call := mockObj.On("DoSomething", 42).Return(true)
 
-	// Create an unexpected expectation
 	unexpected := Unexpected()
-
-	// This should call Unset on the call
 	unexpected.Expectorise(call)
 
-	// Verify that the call was properly unset
 	mockObj.AssertExpectations(t)
-	assert.Empty(t, mockObj.ExpectedCalls)
+	is.Empty(mockObj.ExpectedCalls)
 }
